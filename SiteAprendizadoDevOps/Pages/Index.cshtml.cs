801a5957b73e1694b7eeb9985770d05f07c0e310
Renato Groffe <renato.groffe@yahoo.com.br>
2021-08-30 16:59:28 -0300

    .NET 5 + ASP.NET Core + Razor Pages + Application Insights + Logging + Docker + Linux
    .NET 5 + ASP.NET Core + Razor Pages + Application Insights + Logging + Docker + Linux

---
+++ SiteAprendizadoDevOps/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace SiteAprendizadoDevOps.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private const string SAUDACAO = "Olá!";
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            TempData["Saudacao"] = SAUDACAO;
+
+            // Exibir horário no padrão brasileiro
+            _logger.LogInformation(
+                $"Requisição recebida em: {DateTime.UtcNow.AddHours(-3):HH:mm:ss.ffff} | " +
+                $"Saudação: {SAUDACAO}");
+        }
+    }
+}
